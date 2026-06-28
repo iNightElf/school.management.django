754704a3e98d6c20ea93a5086d2ef858c4d06399
Shahzad <hope_charm_angel@yahoo.com>
2026-06-28 23:38:32 +0600

    use os.environ directly in VAPID view

--- parents/urls.py
+++ parents/urls.py
@@ -1,5 +1,5 @@
+import os
 from django.urls import path
-from django.conf import settings
 from django.http import JsonResponse
 from . import views
 
@@ -9,6 +9,6 @@ urlpatterns = [
     path('parents/fees/<uuid:student_id>/', views.StudentFeesView.as_view(), name='parent-student-fees'),
     path('parents/results/<uuid:student_id>/', views.StudentResultsView.as_view(), name='parent-student-results'),
     path('parents/push/subscribe/', views.PushSubscribeView.as_view(), name='push-subscribe'),
-    path('parents/push/vapid-key/', lambda r: JsonResponse({'publicKey': settings.VAPID_PUBLIC_KEY}), name='vapid-key'),
+    path('parents/push/vapid-key/', lambda r: JsonResponse({'publicKey': os.environ.get('VAPID_PUBLIC_KEY', '')}), name='vapid-key'),
     path('parents/announcements/', views.AnnouncementListView.as_view(), name='parent-announcements'),
 ]
