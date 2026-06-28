e2557c07798bd53dbc775aa72e149756ffdc83a3
Shahzad <hope_charm_angel@yahoo.com>
2026-06-28 23:50:37 +0600

    remove auth from VapidKeyView (public key)

--- parents/views.py
+++ parents/views.py
@@ -255,11 +255,10 @@ class PushSubscribeView(APIView):
 
 
 class VapidKeyView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = []
 
     def get(self, request):
         val = os.environ.get('VAPID_PUBLIC_KEY', '')
-        logger.info("VAPID_PUBLIC_KEY=%s", val)
         return Response({'publicKey': val})
 
 
